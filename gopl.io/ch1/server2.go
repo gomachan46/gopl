4532718ee7d63bc3d59291f406353d189e7ad455
gomachan46 <shiro.gomachan46@gmail.com>
2016-07-28 21:04:30 +0900

    server3: 排他処理, 複数ハンドラ, ロック簡単便利

---
+++ gopl.io/ch1/server2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handlerは、リクエストされたURL rのPath要素を返す
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+// mu.Lock()とmu.Unlock()で排他処理が出来る
+// ロック機構便利
+
