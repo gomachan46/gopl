6449966792cd5c75da1ca89c08dc45e162a01408
gomachan46 <shiro.gomachan46@gmail.com>
2016-08-02 23:15:54 +0900

    タグなしswitch: それぞれがブーリアンの時使える

--- gopl.io/ch1/switch.go
+++ gopl.io/ch1/switch.go
@@ -15,15 +15,30 @@ func main() {
   default:
     fmt.Println("landed on edge!")
   }
+  fmt.Println(janken())
 }
 
 func coinflip() string {
   rand.Seed(time.Now().UnixNano())
   n := rand.Intn(2)
-  fmt.Println(n)
   if n > 0 {
     return "heads"
   } else {
     return "tails"
+  }
+}
+
+func janken() string {
+  rand.Seed(time.Now().UnixNano())
+  n := rand.Intn(3)
+  switch {
+  case n == 0:
+    return "グー"
+  case n == 1:
+    return "チョキ"
+  case n == 2:
+    return "パー"
+  default:
+    return "反則奴〜"
   }
 }
